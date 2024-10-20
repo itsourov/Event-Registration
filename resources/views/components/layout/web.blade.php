bd5dc7cea6c7820f4b83794042e44143d493da39
Sourov Biswas <itsourov12@gmail.com>
2024-10-20 18:20:38 +0600

    homepage

---
+++ resources/views/components/layout/web.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+
+
+
+    @vite('resources/css/app.css')
+
+
+</head>
+
+<body class="bg-fixed bg-cover font-poppins " style="background-image: url('{{ asset('images/backdrop.png') }}')">
+
+
+{{ $slot }}
+
+@vite('resources/js/app.js')
+</body>
+</html>
