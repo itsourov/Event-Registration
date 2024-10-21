d1af7fc12e26344ade02a58399e3e5fe693c35b0
Sourov Biswas <itsourov12@gmail.com>
2024-10-21 12:57:05 +0600

    payment

--- app/Http/Controllers/Payment/RegistrationPaymentController.php
+++ app/Http/Controllers/Payment/RegistrationPaymentController.php
@@ -84,6 +84,7 @@ class RegistrationPaymentController extends Controller
                     return "Information Mismatched, Please contact someone from DIU ACM. Dont worry your payment wont be lost.";
                 }
             } else {
+                return $response->json();
                 return "There was an error connecting to the Payment API. Dont get worried, you wont loss your payment";
             }
         } catch (ConnectionException $e) {
