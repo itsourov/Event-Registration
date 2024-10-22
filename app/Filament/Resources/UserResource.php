55e93144e1d2a745c34271ebc25f1f8e7395e97e
Sourov Biswas <itsourov12@gmail.com>
2024-10-22 11:49:41 +0600

    ok

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -100,4 +100,22 @@ class UserResource extends Resource
                 SoftDeletingScope::class,
             ]);
     }
+    public static function getPermissionPrefixes(): array
+    {
+        return [
+            'view',
+            'view_any',
+            'create',
+            'update',
+            'restore',
+            'restore_any',
+            'replicate',
+            'reorder',
+            'delete',
+            'delete_any',
+            'force_delete',
+            'force_delete_any',
+            'manage_role'
+        ];
+    }
 }
