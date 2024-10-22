4ad74043e9b31fc09656f7e0aba73dbee4852f44
Sourov Biswas <itsourov12@gmail.com>
2024-10-22 13:58:17 +0600

    header link

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,9 +1,10 @@
 <header class="mt-[2%] z-10 ">
     <div class="flow-root">
         <div class="float-left md:w-[60%] mb-6 lg:mb-0">
-            <div class="flex flex-row items-center"><img src="{{ asset('images/uta-logo.webp') }}"
-                                                         class="w-28 h-14 mb-2 md:ml-[5%] ml-4 cursor-pointer"
-                                                         alt="Logo">
+            <a href="{{route('home')}}" class="flex flex-row items-center"><img
+                        src="{{ asset('images/uta-logo.webp') }}"
+                        class="w-28 h-14 mb-2 md:ml-[5%] ml-4 cursor-pointer"
+                        alt="Logo">
                 <div class="flex flex-col cursor-pointer">
                     <div class="text-lg md:text-3xl leading-normal text-cyan-900 font-semibold ml-5">
                         {{$site_settings->site_title}}
@@ -14,12 +15,12 @@
                         </blockquote>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
         <div
-            class="w-full lg:w-[40%] mb-6 lg:mb-0 h-16 bg-cyan-900 rounded lg:rounded-l-full lg:pl-10 px-4 flex items-center justify-start">
+                class="w-full lg:w-[40%] mb-6 lg:mb-0 h-16 bg-cyan-900 rounded lg:rounded-l-full lg:pl-10 px-4 flex items-center justify-start">
             <ul
-                class="w-full inline-flex cursor-pointer items-center justify-around md:justify-between gap-x-3 md:gap-x-6 space-x-reverse  text-white font-normal flex-wrap text-base">
+                    class="w-full inline-flex cursor-pointer items-center justify-around md:justify-between gap-x-3 md:gap-x-6 space-x-reverse  text-white font-normal flex-wrap text-base">
                 <li><a href="{{route('home')}}">Home</a></li>
                 <li><a href="{{route('all-registrations')}}">All Registrations</a></li>
                 <li><a href="{{route('contact')}}">Contact</a></li>
