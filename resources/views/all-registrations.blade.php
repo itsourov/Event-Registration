4866e4694701f972c1460b1ed4623348c6f87393
Sourov Biswas <itsourov12@gmail.com>
2024-10-22 10:33:18 +0600

    ok

--- resources/views/all-registrations.blade.php
+++ resources/views/all-registrations.blade.php
@@ -1,7 +1,7 @@
 <x-web-layout>
     @include('inc.header')
 
-    <div class="mt-[2%]">
+    <div class="mt-[2%] px-[2%] mx-auto">
         <livewire:registration-list/>
     </div>
 </x-web-layout>
