32859bff579fee4b31e9fe9a46f027861a93f485
Sourov Biswas <itsourov12@gmail.com>
2024-10-31 00:51:04 +0600

    message

--- resources/views/registration/my-registration.blade.php
+++ resources/views/registration/my-registration.blade.php
@@ -4,7 +4,7 @@
 
 
         <x-card class=" p-4 space-y-1 ">
-             <div class="max-w-md mx-auto">
+            <div class="max-w-md mx-auto">
                 <div class="aspect-w-16 aspect-h-9">
                     {{ $registration->contest->getFirstMedia("contest-banner-images") ?->img()->attributes(["class" => "w-full object-cover rounded-xl"]) }}
                 </div>
@@ -40,9 +40,9 @@
             <p>
                 Lab Teacher Name: <span class="font-semibold">{{$registration->lab_teacher_name}}</span>
             </p>
-{{--            <p>--}}
-{{--                Charged amount: <span class="font-semibold">{{$registration->extra['charged_amount']??''}}</span>--}}
-{{--            </p>--}}
+            {{--            <p>--}}
+            {{--                Charged amount: <span class="font-semibold">{{$registration->extra['charged_amount']??''}}</span>--}}
+            {{--            </p>--}}
             <p>
                 payment method: <span class="font-semibold">{{$registration->payment_method}}</span>
             </p>
@@ -52,16 +52,21 @@
             <p>
                 Transaction id: <span class="font-semibold">{{$registration->payment_transaction_id}}</span>
             </p>
-{{--            <div class="text-success-500 text-info-500"></div>--}}
+            {{--            <div class="text-success-500 text-info-500"></div>--}}
             <p>
-                Registration status: <span class="font-semibold text-lg text-{{$registration->status->getColor()}}-500">{{$registration->status}}</span>
+                Registration status: <span
+                    class="font-semibold text-lg text-{{$registration->status->getColor()}}-500">{{$registration->status}}</span>
             </p>
 
 
         </x-card>
 
-        <div class="mt-4 bg-blue-100 border border-blue-200 text-sm text-blue-800 rounded-lg p-4 dark:bg-blue-800/10 dark:border-blue-900 dark:text-blue-500" role="alert" tabindex="-1" aria-labelledby="hs-soft-color-info-label">
-            <span id="hs-soft-color-info-label" class="font-bold">Attention.</span> Please wait up to 24 hours if your payment is not reflected here.
-        </div>
+        @if($registration->status==\App\Enums\RegistrationStatuses::PENDING)
+            <div
+                class="mt-4 bg-blue-100 border border-blue-200 text-sm text-blue-800 rounded-lg p-4 dark:bg-blue-800/10 dark:border-blue-900 dark:text-blue-500"
+                role="alert" tabindex="-1" aria-labelledby="hs-soft-color-info-label">
+                Pending means you submitted the form successfully. Now we will verify your payment and update the registration status. It may take some time because we are checking everything manually.
+            </div>
+        @endif
     </div>
 </x-web-layout>
