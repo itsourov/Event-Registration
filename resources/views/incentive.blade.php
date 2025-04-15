527a5c4d12e3c601a823fcebe572a61818571f39
Sourov Biswas <sourov2305101004@diu.edu.bd>
2025-04-15 11:39:07 +0600

    refactor incentive page layout for improved styling and user experience

--- resources/views/incentive.blade.php
+++ resources/views/incentive.blade.php
@@ -1,7 +1,32 @@
 <x-web-layout>
-    <div class="container mx-auto">
-        <livewire:incentive-form/>
+    <div class="py-12 bg-gray-50 dark:bg-gray-900 min-h-screen">
+        <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="max-w-4xl mx-auto">
+                <!-- Header with gradient background -->
+                <div class="text-center mb-10 bg-gradient-to-r from-blue-600 to-indigo-700 dark:from-blue-800 dark:to-indigo-900 p-10 rounded-lg shadow-lg">
+                    <h1 class="text-4xl font-bold text-white mb-3">Incentive Application</h1>
+                    <p class="text-lg text-gray-100">
+                        Complete this form to submit your course incentive application
+                    </p>
+                </div>
+
+                <!-- Form Card with improved styling -->
+                <div class="bg-white dark:bg-gray-800 rounded-xl shadow-xl overflow-hidden border border-gray-200 dark:border-gray-700 transition-all duration-300">
+                    <div class="p-8">
+                        <livewire:incentive-form/>
+                    </div>
+                </div>
+
+                <!-- Additional Information with dark mode support -->
+                <div class="mt-10 bg-white dark:bg-gray-800 rounded-xl shadow-lg p-8 border border-gray-200 dark:border-gray-700 transition-all duration-300">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white mb-4">Important Information</h3>
+                    <ul class="list-disc pl-6 text-gray-600 dark:text-gray-300 space-y-3">
+                        <li>Make sure to provide accurate teacher information</li>
+                        <li>All fields are required to process your application</li>
+                        <li>You will receive a confirmation email after successful submission</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-
-
 </x-web-layout>
--- resources/views/livewire/incentive-form.blade.php
+++ resources/views/livewire/incentive-form.blade.php
@@ -1,13 +1,17 @@
-<div class="container mx-auto p-4">
-    <form wire:submit="create" class="sourov">
-        {{ $this->form }}
+<div class="w-full">
+    <form wire:submit="create" class="space-y-8">
+        <div class="bg-gray-50 dark:bg-gray-800/30 p-6 rounded-lg border border-gray-100 dark:border-gray-700">
+            {{ $this->form }}
+        </div>
 
-        <button type="submit" class="mt-4 px-6 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg transition-colors duration-200 flex items-center justify-center">
-            <span>Submit</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-            </svg>
-        </button>
+        <div class="flex justify-center">
+            <button type="submit" class="px-8 py-3 bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 dark:from-blue-700 dark:to-indigo-800 dark:hover:from-blue-800 dark:hover:to-indigo-900 text-white font-medium rounded-lg transition-all duration-200 shadow-md hover:shadow-lg flex items-center justify-center group">
+                <span class="text-lg">Submit Application</span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2 transform group-hover:translate-x-1 transition-transform duration-200" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
     </form>
 
     <x-filament-actions::modals />
